37b2b8459ea03200f022a28ae04bd449728f0cdf
Mate Torok <mate.torok.de@gmail.com>
2017-09-30 12:22:36 +0200

    Added EmailAction component

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RaspberryPi Security
 This project is about a home-security system that is based on RaspberryPi. It aims to provide a configurable and extensible framework that can easily accommodate new features made by developers.
+
+## Architecture
---
+++ raspberry_sec/module/email/action.py
@@ -0,0 +1,44 @@
+import logging
+import smtplib
+from raspberry_sec.interface.action import Action
+
+
+class EmailAction(Action):
+	"""
+	Action class for sending emails
+	"""
+	LOGGER = logging.getLogger('EmailAction')
+	FROM_ADDR = 'mt.raspberry.pi@gmail.com'
+	TO_ADDR = 'mate.torok.de@gmail.com'
+	SMTP_ADDR = 'smtp.gmail.com:587'
+	SMTP_TIMEOUT = 10
+	USER = 'mt.raspberry.pi'
+	PASSWORD = 'MT_raspberry'
+
+	def get_name(self):
+		return 'EmailAction'
+
+	def fire(self, msg: str):
+		"""
+		This method sends an email with the content set to the msg.
+		:param msg: email content
+		"""
+		EmailAction.LOGGER.info('Action fired: ' + msg)
+
+		mail = "\n".join([
+			'From: ' + EmailAction.FROM_ADDR,
+			'To: ' + EmailAction.TO_ADDR,
+			'Subject: RaspberryPi ALERT',
+			'',
+			msg
+		])
+
+		try:
+			with smtplib.SMTP(host=EmailAction.SMTP_ADDR, timeout=EmailAction.SMTP_TIMEOUT) as server:
+				server.ehlo()
+				server.starttls()
+				server.login(EmailAction.USER, EmailAction.PASSWORD)
+				server.sendmail(EmailAction.FROM_ADDR, EmailAction.TO_ADDR, mail)
+			EmailAction.LOGGER.info('Email has been successfully dispatched')
+		except Exception:
+			EmailAction.LOGGER.error('Email could not be dispatched')
