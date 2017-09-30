68cc4465fdb6365cf5d9111907cdd3d0ae2c3eeb
Mate Torok <mate.torok.de@gmail.com>
2017-09-30 20:01:08 +0200

    pytest added to make.sh

--- make.sh
+++ make.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo '=== Compilation of source files'
 for script in $( find . -name "*.py" );
 do
     echo "Compiling: ${script}"
     python -m py_compile $script
 done
+
+
+echo '=== Running unit-tests'
+pytest .
