f6ce7e37a0b9e66fc410706fc66d013ff16c2b8c
Mate Torok <mate.torok.de@gmail.com>
2017-10-01 11:49:15 +0200

    Fixed make.sh

--- make.sh
+++ make.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
+# ---------------------------------------------
 echo '=== Compilation of source files'
+errors=0
+
 for script in $( find . -name "*.py" );
 do
-    echo "Compiling: ${script}"
+    echo 'Compiling: ' $script
     python -m py_compile $script
+
+    if [ 0 -ne $? ]
+    then
+        ((errors=errors+1))
+    fi
 done
 
+if [ 0 -ne $errors ]
+then
+    echo 'Failed to compile' $errors 'files'
+	exit $errors
+fi
 
+
+# ---------------------------------------------
 echo '=== Running unit-tests'
 pytest .
+
+# if no tests were found, don't fail
+if [ 5 eq $? ]
+then
+	exit 0
+fi
