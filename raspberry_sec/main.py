3c8ee35faa3901b9eb53139d5fd922df205cde6a
Mate Torok <mate.torok.de@gmail.com>
2017-10-08 12:48:52 +0200

    Fixed parent process logging issue (wasnt setup to use the new facility --> no logging)

--- raspberry_sec/main.py
+++ raspberry_sec/main.py
@@ -6,7 +6,7 @@ from threading import Thread
 from multiprocessing import Process, Event, Queue
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from raspberry_sec.pca import PCASystemJSONDecoder
-from raspberry_sec.util import LogQueueListener, ProcessContext
+from raspberry_sec.util import LogQueueListener, ProcessContext, ProcessReady
 
 
 def run_pcasystem():
@@ -23,17 +23,22 @@ def run_pcasystem():
 		logging_process = Process(target=log_listener.run, args=(logging_queue,))
 		logging_process.start()
 
+		# Setup logging for current process
+		ProcessReady.setup_logging(logging_queue)
+
 		pca_system = PCASystemJSONDecoder.load_from_config('../config/pca_system.json')
 		event = Event()
 		event.clear()
 
 		process_context = ProcessContext(log_queue=logging_queue, stop_event=event)
-		Thread(target=pca_system.start, args=(process_context, )).start()
+		pca_process = Process(target=pca_system.start, args=(process_context, ))
+		pca_process.start()
 
 		time.sleep(30)
 	finally:
 		event.set()
 		time.sleep(0.5)
+		pca_process.terminate()
 		logging_process.terminate()
 
 
