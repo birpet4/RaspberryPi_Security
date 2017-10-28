c8f78e3fdd7d9350cd28df479e33d16825643da4
Mate Torok <mate.torok.de@gmail.com>
2017-10-28 22:11:22 +0200

    Refactored integration test for this module: email

--- raspberry_sec/module/email/test.py
+++ raspberry_sec/module/email/test.py
@@ -1,13 +1,23 @@
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
 from raspberry_sec.module.email.action import EmailAction
 from raspberry_sec.interface.action import ActionMessage
 
 
-def system_test():
+def set_parameters():
+	print('Do not forget to set the password !!!')
+	parameters = dict()
+	parameters['password'] = ''
+	parameters['from_addr'] = 'mt.raspberry.pi@gmail.com'
+	parameters['smtp_addr'] = 'smtp.gmail.com:587'
+	parameters['smtp_timeout'] = 10
+	parameters['subject'] = 'RaspberryPi ALERT'
+	parameters['to_addr'] = 'mate.torok.de@gmail.com'
+	parameters['user'] = 'mt.raspberry.pi'
+	return parameters
+
+
+def integration_test():
 	# Given
-	email_action = EmailAction()
+	email_action = EmailAction(set_parameters())
 
 	# When
 	email_action.fire([
@@ -16,4 +26,4 @@ def system_test():
 
 
 if __name__ == '__main__':
-	system_test()
+	integration_test()
